bad5d6175d77bd451d2608065735dea2e6d5278a
LeeXTing <113869024+LeeXTing@users.noreply.github.com>
2022-11-03 16:00:24 +0800

    1103

--- 0929/Assets/scripts/FireSystemBase.cs
+++ 0929/Assets/scripts/FireSystemBase.cs
@@ -11,5 +11,17 @@ namespace selena
         private GameObject prefaBullet;
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
+
+        // 自訂方法
+        // 生成子彈
+        // protected 保護級別 : 允許子類別存取
+        protected void SpawnBullet()
+        {
+            // 實例化(物件，座標，角度)
+            // 生成 子彈預製物 角度與座標跟子彈生成點相同
+            Instantiate(prefaBullet, pointSpawn.position, pointSpawn.rotation);
+
+        }
+
     }
 }
--- morning/1017/Assets/Scripts/PlayerController.cs
+++ morning/1017/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("控制前後速度的數值"), Range(0, 20)]
     public float speed = 20;
 
+    [Header("左右速度")]
     public float trunSpeed = 10;
+
+    [Tooltip("這邊會顯示前後按鍵按下的偵測數值")]
+    public float VInput; //宣告一個變量來存放鍵盤按上下鍵時，取得的(正/負)數值
+    public float H_Input;
+
+    public float CrazyDrive; //開啟喝醉模式
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +25,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+        VInput = Input.GetAxis("Vertical");//取得鍵盤按上下鍵時，取得的(正/負)數值
+        print("前後按鍵" + VInput); //DeBug: 檢查 VInput 中的數值為何
 
-        transform.Translate(Vector3.right * Time.deltaTime * trunSpeed);
+        H_Input = Input.GetAxis("Horizontal");
+        print("左右按鍵" + H_Input); //DeBug: 檢查 H_Input 中的數值為何
+
+
+        //控制車子前後(透過上下鍵, W/S)
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * VInput);
+
+        CrazyDrive = Random.Range(-30, 30); //設定喝醉的瘋狂數值
+        print("CD:" + CrazyDrive);
+
+        //控制車子左右(透過左右鍵, A/D)
+        //transform.Translate(Vector3.right * Time.deltaTime * trunSpeed * H_Input);
+        transform.Rotate(Vector3.up * Time.deltaTime * trunSpeed * H_Input);
     }
 }
